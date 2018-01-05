ece7ca605c0e10ca121625399fb9f3b2fcafdf23
Daniel Ingram <ingramds@appstate.edu>
2018-01-04 22:45:17 -0500

    Adjusted output file formatting for .md

--- sorting/python/algorithm_comparison.py
+++ sorting/python/algorithm_comparison.py
@@ -20,76 +20,61 @@ quick_times = [0]*n_powers
 counting_times = [0]*n_powers
 
 file = open('README.md', 'w')
-file.write('n\t\t\t\t\t')
+file.write('|n|')
 for i in xrange(n_powers):
 	if i is not n_powers-1:
-		file.write("%-10d" % n_elements[i] +'\t\t\t')
-	else:
-		file.write("%-10d" % n_elements[i] +'\r\n\n')
+		file.write(str(n_elements[i]) +'|')
+
+file.write('\r\n\n')
+
+file.write('|---:|')
+for _ in xrange(n_powers):
+	file.write(':---:|')
+file.write('\r\n')
 
 array = [0, 0, 0, 0]
 for i in xrange(n_powers):
 	array[i] = np.random.randint(100, size=n_elements[i])
 
-file.write('Selection sort\t\t')
+file.write('|**Selection sort**|')
 for i in xrange(n_powers):
 	selection_time = timeit(stmt='selection_sort(array[i].copy())', setup="from __main__ import array, selection_sort, i", number=1)
 	selection_times[i] = selection_time
 
-	file.write("%0.2e" % selection_time)
-	
-	if i is not n_powers-1:
-		file.write(' s\t\t\t')
-	else:
-		file.write(' s\r\n')
+	file.write(str(selection_time) + ' s|')
+file.write('\r\n')
 
-file.write('Insertion sort\t\t')
+file.write('|**Insertion sort**|')
 for i in xrange(n_powers):
 	insertion_time = timeit(stmt='insertion_sort(array[i].copy())', setup="from __main__ import array, insertion_sort, i", number=1)
 	insertion_times[i] = insertion_time
 	
-	file.write("%0.2e" % insertion_time)
-	
-	if i is not n_powers-1:
-		file.write(' s\t\t\t')
-	else:
-		file.write(' s\r\n')
+	file.write(str(insertion_time) + ' s|')
+file.write('\r\n')
 
-file.write('Merge sort\t\t\t')
+file.write('|**Merge sort**|')
 for i in xrange(n_powers):
 	merge_time = timeit(stmt='merge_sort(array[i].copy(), 0, n_elements[i]-1)', setup="from __main__ import array, merge_sort, i, n_elements", number=1)
 	merge_times[i] = merge_time
 
-	file.write("%0.2e" % merge_time)
-		
-	if i is not n_powers-1:
-		file.write(' s\t\t\t')
-	else:
-		file.write(' s\r\n')
+	file.write(str(merge_time) + ' s|')
+file.write('\r\n')
 
-file.write('Quick sort\t\t\t')
+file.write('|**Quick sort**|')
 for i in xrange(n_powers):
 	quick_time = timeit(stmt='quick_sort(array[i].copy(), 0, n_elements[i]-1)', setup="from __main__ import array, quick_sort, i, n_elements", number=1)
 	quick_times[i] = quick_time
 
-	file.write("%0.2e" % quick_time)
-	
-	if i is not n_powers-1:
-		file.write(' s\t\t\t')
-	else:
-		file.write(' s\r\n')
+	file.write(str(quick_time) + ' s|')
+file.write('\r\n')
 
-file.write('Counting sort\t\t')
+file.write('|**Counting sort**|')
 for i in xrange(n_powers):
 	counting_time = timeit(stmt='counting_sort(array[i].copy())', setup="from __main__ import array, counting_sort, i", number=1)
 	counting_times[i] = counting_time
 
-	file.write("%0.2e" % counting_time)
-	
-	if i is not n_powers-1:
-		file.write(' s\t\t\t')
-	else:
-		file.write(' s\r\n')
+	file.write(str(counting_time) + ' s|')
+file.write('\r\n')
 
 file.close()
 
